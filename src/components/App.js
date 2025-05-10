c4cf9a5971968259245f9969e94166cca9527167
Durga Prasad <saikamireddi3@gmail.com>
2025-05-10 11:48:41 +0530

    text has changed

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import PageLayout from "./PageLayout";
 const App = () => {
   return (
     <div>
+      {/* Do not remove the main div */}
       <PageLayout
         header={<h1>Welcome to my website</h1>}
         footer={<p>&copy; 2023 My Website. All rights reserved.</p>}
